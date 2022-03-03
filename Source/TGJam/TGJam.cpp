1a3f8eda27c2b568aa75513944083f7a7ee5a093
Cesar Guayara <cguayara@teravisiongames.com>
2022-03-03 14:08:51 -0500

    Base project setup

---
+++ Source/TGJam/TGJam.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TGJam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TGJam, "TGJam" );
+
+DEFINE_LOG_CATEGORY(LogTGJam)
+ 
