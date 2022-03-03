65ea6b280bc275ea1094f69b459e920b91157ca2
Cesar Guayara <cguayara@teravisiongames.com>
2022-03-03 17:23:16 -0500

    Base classes setup

---
+++ Source/TGJam/Public/GameFramework/TGJ_GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TGJ_GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TGJAM_API ATGJ_GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TGJam/Public/GameplayAbilitySystem/TGJ_AttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "TGJ_AttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TGJAM_API UTGJ_AttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
