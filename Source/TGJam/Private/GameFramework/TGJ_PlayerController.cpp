54329c277ac4eff757de342364cc4dd1ed0811ae
Cesar Guayara <cguayara@teravisiongames.com>
2022-03-09 16:45:12 -0500

    Unrealiable but functional Multiplayer

--- Source/TGJam/Private/GameFramework/TGJ_PlayerController.cpp
+++ Source/TGJam/Private/GameFramework/TGJ_PlayerController.cpp
@@ -14,23 +14,16 @@ ATGJ_PlayerController::ATGJ_PlayerController()
 }
 
 //=================================================================================================================
-void ATGJ_PlayerController::PlayerTick(float DeltaTime)
+void ATGJ_PlayerController::Tick(float DeltaTime)
 {
-	Super::PlayerTick(DeltaTime);
+	Super::Tick(DeltaTime);
 
 	// keep updating the destination every tick while desired
-	if (bMoveToMouseCursor)
+	// TODO Find the optimal way to replicate Cursor's movement, I hate Server RPCs on Tick
+	if (bMoveToCursor)
 	{
 		MoveToMouseCursor();
 	}
-}
-
-//=================================================================================================================
-void ATGJ_PlayerController::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
-{
-	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
-
-	DOREPLIFETIME(ATGJ_PlayerController, LastCursorLocation);
 }
 
 //=================================================================================================================
@@ -60,15 +53,15 @@ void ATGJ_PlayerController::MoveToMouseCursor()
 void ATGJ_PlayerController::SetNewMoveDestination(const FVector DestLocation)
 {
 	const APawn* MyPawn = GetPawn();
-	if (MyPawn && HasAuthority())
+	if (MyPawn)
 	{
 		float const Distance = FVector::Dist(DestLocation, MyPawn->GetActorLocation());
 
 		// We need to issue move command only if far enough in order for walk animation to play correctly
 		if ((Distance > 120.0f))
 		{
+			Server_SetLastCursorLocation(DestLocation);
 			UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, DestLocation);
-			LastCursorLocation = DestLocation;
 		}
 	}
 }
@@ -77,18 +70,18 @@ void ATGJ_PlayerController::SetNewMoveDestination(const FVector DestLocation)
 void ATGJ_PlayerController::OnSetDestinationPressed()
 {
 	// set flag to keep updating destination until released
-	bMoveToMouseCursor = true;
+	bMoveToCursor = true;
 }
 
 //=================================================================================================================
 void ATGJ_PlayerController::OnSetDestinationReleased()
 {
 	// clear flag to indicate we should stop updating the destination
-	bMoveToMouseCursor = false;
+	bMoveToCursor = false;
 }
 
 //=================================================================================================================
-void ATGJ_PlayerController::OnRep_LastCursorLocation(const FVector& OldLastCursorLocation)
+void ATGJ_PlayerController::Server_SetLastCursorLocation_Implementation(const FVector& LastCursorLocation)
 {
 	UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, LastCursorLocation);
 }
